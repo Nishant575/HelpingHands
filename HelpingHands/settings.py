77ff915b260f41582f3e994614e8a6892a6eff6a
Nishant575 <n.dalvi2010@gmail.com>
2021-10-27 20:46:50 +0530

    commit

--- HelpingHands/settings.py
+++ HelpingHands/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hh.apps.HhConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
